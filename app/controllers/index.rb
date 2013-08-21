447c8f723a51ec116f58d392488694e5d24646b0
mehagel65@gmail.com <mehagel65@gmail.com>
2013-08-21 08:07:52 -0700

    first commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,16 @@
+# require 'pry'
+
+get '/' do
+	@project = []
+	url = 'http://bxwa.com/bxwa_toc/pub/1661/toc.html'
+	data =Nokogiri::HTML(open(url))
+	@plans = data.css('a')
+
+	@project = data.css('tr :nth-child(2)')
+	p @project
+	# p @project.push (@project_object.text)
+
+	# binding.pry
+  # Look in app/views/index.erb
+  erb :index
+end
