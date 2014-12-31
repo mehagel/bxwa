9c2fe4e849c5ddd9ff8422400abcb8c359476771
Mark H <mehagel65@gmail.com>
2014-12-31 07:55:38 -0800

    migration tables

--- db/migrate/20130821133818_create_project_table.rb
+++ db/migrate/20130821133818_create_project_table.rb
@@ -3,17 +3,17 @@ class CreateProjectTable < ActiveRecord::Migration
   def change
   	create_table :projects do |t|
   		t.string :project_name
-  		t.string :plans
-  		t.string :url
+  		t.string :bid_date
   		t.string :city
   		t.string :state
-  		t.string :bid_date
       t.string :classification
       t.string :url
-      t.string :city
-      t.string :state
-      t.string :bid_date
-      t.string :classification
+      t.string :owner
+      t.string :AE
+      t.string :notes
+      t.string :description
+      t.string :engineer_estimate
+      t.string :contact
   		t.timestamps
   	end
   end
