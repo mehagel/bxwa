baa121a533759662f9f547875324a72e42933ec7
mehagel <mehagel65@gmail.com>
2016-06-07 19:23:01 -0700

    updates

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,7 @@
 
 get '/' do
 	@agency = Agency.all.sort
-	@project=Projects.all
+	@projects=Projects.all.sort
 
   erb :index
 end
--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,9 +1,9 @@
 class Agency < ActiveRecord::Base
   # attr_accessor :name, :url
-  # validates :url, uniqueness: true
-  # validates :name, uniqueness: true
-  # validates :name, presence: true
-  # has_many  :projects
+  validates :url, uniqueness: true
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  has_many  :projects
 end
 
 	def get_agencies
